11bf7c273631b5654bca6b5af1aeaab4331d1ff2
Bac Do <bac.do@evizi.com>
2017-08-05 12:14:49 +0700

    update Layout for show a car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,6 @@
 class CarsController < ApplicationController
   before_action :set_car, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :new, :create]
 
   # GET /cars
   # GET /cars.json
